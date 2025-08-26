7d8cfc4783fac812b2973289f5528b6a18f5c6b3
ProficientPug <rohith200345@gmail.com>
2025-08-27 01:34:06 +0530

    removed duplicate router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';

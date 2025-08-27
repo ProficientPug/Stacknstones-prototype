3a22c77d3b146373964c2b91a37b8f3c48b0f48d
ProficientPug <rohith200345@gmail.com>
2025-08-27 14:45:29 +0530

    Fixed Projects Page

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -23,7 +23,7 @@ function ProjectGallery() {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/projects');
+        const response = await axios.get('https://stacknstones-prototype.onrender.com');
         setProjects(response.data);
       } catch (error) {
         console.error("Error fetching projects:", error);

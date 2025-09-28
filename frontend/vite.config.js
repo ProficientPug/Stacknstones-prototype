40390d8158f0070984dcf0bbf50650947b0617c3
ProficientPug <rohith200345@gmail.com>
2025-09-28 20:18:57 +0530

    fixing vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   // Add this server configuration
   server: {
     sourcemap: 'inline-source-map',

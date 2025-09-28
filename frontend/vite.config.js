9e711feb3f336555e9c224308ff044c36317de3a
ProficientPug <rohith200345@gmail.com>
2025-09-28 20:28:59 +0530

    fixing vite file #2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Stacknstones-prototype/',
   plugins: [react()],
-  base: '/', 
-  // Add this server configuration
-  server: {
-    sourcemap: 'inline-source-map',
-  },
-});
+})

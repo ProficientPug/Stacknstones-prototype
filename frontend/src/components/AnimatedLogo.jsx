65a91594c402d1fc3494c95bb65a081b6b422b75
ProficientPug <rohith200345@gmail.com>
2025-11-05 20:13:54 +0530

    fixing logo import

--- frontend/src/components/AnimatedLogo.jsx
+++ frontend/src/components/AnimatedLogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './AnimatedLogo.css'; // Import your new CSS file
-import { ReactComponent as Logo } from '../assets/logo_final.svg';
+import './AnimatedLogo.css';
+import Logo from '../assets/logo_final.svg?react'
 
 const AnimatedLogo = () => {
   // This renders the SVG inline, allowing the CSS to target its internal classes
